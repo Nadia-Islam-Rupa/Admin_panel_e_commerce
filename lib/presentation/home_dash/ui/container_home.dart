bc8f6d3ca0bca82960c56c5361cefd585bb3ca0a
Nadia Islam <sohasara853@gmail.com>
2026-02-09 11:13:19 +0600

    feat: enhance ContainerHome layout with user statistics display

--- lib/presentation/home_dash/ui/container_home.dart
+++ lib/presentation/home_dash/ui/container_home.dart
@@ -7,11 +7,20 @@ class ContainerHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(8),
       height: 100,
       width: 180,
       decoration: BoxDecoration(
         color: color,
         borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [Text("Total Users"), Text("100")],
+          ),
+        ],
       ),
     );
   }
