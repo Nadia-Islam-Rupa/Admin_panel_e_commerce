fac87c58e26ca278d7dabd8981d66dfd4f8dd803
Nadia Islam <sohasara853@gmail.com>
2026-02-11 19:56:45 +0600

    feat: create EditCategory screen with basic layout

---
+++ lib/presentation/category/ui/edit_category.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EditCategory extends StatelessWidget {
+  const EditCategory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(children: [
+        
+      ],),
+    );
+  }
+}
