e5da0981308785bc60bb33fc6d9b49d5cc2def51
Nadia Islam <sohasara853@gmail.com>
2026-02-11 20:06:42 +0600

    feat: enhance EditCategory screen layout with padding and structured elements

--- lib/presentation/category/ui/edit_category.dart
+++ lib/presentation/category/ui/edit_category.dart
@@ -6,10 +6,39 @@ class EditCategory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(centerTitle: true, title: const Text('Edit Category')),
-      body: Column(children: [
-        
-      ],),
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+        title: const Text('Edit Category'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Category Name',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              height: 150,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+
+                border: Border.all(width: 0.5),
+              ),
+              child: const Center(child: Text('Image ')),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(onPressed: () {}, child: const Text('Save')),
+          ],
+        ),
+      ),
     );
   }
 }
