1dfd8ec2289c8b0b70a0cf07b4122e931cb5f2fc
Nadia Islam <sohasara853@gmail.com>
2026-02-11 11:58:01 +0600

    change

--- lib/presentation/category/ui/category_card.dart
+++ lib/presentation/category/ui/category_card.dart
@@ -7,7 +7,7 @@ class CategoryCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(12),
-
+      height: 150,
       width: 180,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
@@ -17,7 +17,15 @@ class CategoryCard extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          SizedBox(height: 80, child: Image.network("url")),
+          SizedBox(
+            height: 80,
+            width: 180,
+
+            child: Image.network(
+              "https://becs-table.com.au/wp-content/uploads/2014/01/ice-cream-1.jpg",
+              fit: BoxFit.cover,
+            ),
+          ),
 
           SizedBox(height: 10),
           Text("Total Users", style: TextStyle(fontSize: 18)),
