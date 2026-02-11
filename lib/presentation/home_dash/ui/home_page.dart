b2fcb212d27a7dbcf6ca9225ce8fe0d08ed66a3b
Nadia Islam <sohasara853@gmail.com>
2026-02-11 13:02:43 +0600

    feat: enhance HomePage layout; add "Add New Product" button and adjust category management section

--- lib/presentation/home_dash/ui/home_page.dart
+++ lib/presentation/home_dash/ui/home_page.dart
@@ -13,63 +13,99 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.amber,
         title: Text("Admin Dashboard"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ContainerHome(color: Colors.blue[300]),
-                ContainerHome(color: Colors.teal[400]),
-              ],
-            ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ContainerHome(color: Colors.orange[300]),
-                ContainerHome(color: Colors.purple[400]),
-              ],
-            ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Manage Categories",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ContainerHome(color: Colors.blue[300]),
+                  ContainerHome(color: Colors.teal[400]),
+                ],
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ContainerHome(color: Colors.orange[300]),
+                  ContainerHome(color: Colors.purple[400]),
+                ],
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Manage Categories",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
 
-                TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.blue[100],
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.blue[100],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      "Add New Category",
+                      style: TextStyle(fontSize: 16, color: Colors.black),
                     ),
                   ),
-                  onPressed: () {},
-                  child: Text(
-                    "Add New Category",
-                    style: TextStyle(fontSize: 16, color: Colors.black),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 10),
-            SizedBox(
-              height: 190,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 4,
-                itemBuilder: (context, index) {
-                  return CategoryCard();
-                },
+                ],
               ),
-            ),
-            SizedBox(height: 20),
-          ],
+              SizedBox(height: 10),
+              SizedBox(
+                height: 190,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 4,
+                  itemBuilder: (context, index) {
+                    return CategoryCard();
+                  },
+                ),
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Manage Products",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.blue[100],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      "Add New Product",
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              SizedBox(
+                height: 190,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 4,
+                  itemBuilder: (context, index) {
+                    return CategoryCard();
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
